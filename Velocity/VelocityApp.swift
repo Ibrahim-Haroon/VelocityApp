60666a10cfc9796d6647533d433a2178d5a8eee2
Ibrahim Haroon <ibrahim.haroon140@gmail.com>
2023-10-28 18:57:05 -0400

    initiated auth and backend using firebase. Added GoogleServices file.

--- Velocity/VelocityApp.swift
+++ Velocity/VelocityApp.swift
@@ -5,10 +5,14 @@
 //  Created by Ibrahim Haroon on 10/28/23.
 //
 
+import FirebaseCore
 import SwiftUI
 
 @main
 struct VelocityApp: App {
+    init() {
+        FirebaseApp.configure()
+    }
     var body: some Scene {
         WindowGroup {
             ContentView()
