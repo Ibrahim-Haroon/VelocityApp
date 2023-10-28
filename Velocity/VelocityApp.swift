4522eef48e7ec108e7dbb44b2ac7da2539344401
Ibrahim Haroon <ibrahim.haroon140@gmail.com>
2023-10-28 18:26:02 -0400

    initial commit.

---
+++ Velocity/VelocityApp.swift
@@ -0,0 +1,17 @@
+//
+//  VelocityApp.swift
+//  Velocity
+//
+//  Created by Ibrahim Haroon on 10/28/23.
+//
+
+import SwiftUI
+
+@main
+struct VelocityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
