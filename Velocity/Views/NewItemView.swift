04d9e00171c7fa963e58ca42cc294c0d94c8afc0
Ibrahim Haroon <ibrahim.haroon140@gmail.com>
2023-10-28 19:55:31 -0400

    added views for various instances within app. Views not yet defined.

---
+++ Velocity/Views/NewItemView.swift
@@ -0,0 +1,18 @@
+//
+//  NewItemView.swift
+//  Velocity
+//
+//  Created by Ibrahim Haroon on 10/28/23.
+//
+
+import SwiftUI
+
+struct NewItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NewItemView()
+}
---
+++ Velocity/Views/ToDoListView.swift
@@ -0,0 +1,18 @@
+//
+//  ToDoListView.swift
+//  Velocity
+//
+//  Created by Ibrahim Haroon on 10/28/23.
+//
+
+import SwiftUI
+
+struct ToDoListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ToDoListView()
+}
