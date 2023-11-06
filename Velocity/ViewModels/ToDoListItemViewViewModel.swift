6757ec6710b3e612470a4fc7950064482a42d612
Ibrahim Haroon <ibrahim.haroon140@gmail.com>
2023-11-06 17:44:23 -0500

    implemented marking tasks as done and viewing created tasks.

--- Velocity/ViewModels/ToDoListItemViewViewModel.swift
+++ Velocity/ViewModels/ToDoListItemViewViewModel.swift
@@ -5,9 +5,27 @@
 //  Created by Ibrahim Haroon on 10/28/23.
 //
 
+import FirebaseAuth
+import FirebaseFirestore
 import Foundation
 
 class ToDoListItemViewViewModel: ObservableObject {
     init() {}
     
+    func toggleDone(item: ToDoListItem) {
+        var itemCopy = item
+        itemCopy.setDone(!item.isDone)
+        
+        
+        guard let uid = Auth.auth().currentUser?.uid else {
+            return
+        }
+        
+        let db = Firestore.firestore()
+        db.collection("users")
+            .document(uid)
+            .collection("TODOs")
+            .document(itemCopy.id)
+            .setData(itemCopy.asDictionary())
+    }
 }
